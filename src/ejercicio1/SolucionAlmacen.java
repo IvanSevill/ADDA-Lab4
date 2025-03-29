e8ea9c4b95423d5f50a9803055f1cd1fc93996fc
Ivan Sevillano <ivansevillano2005@gmail.com>
2025-03-29 20:37:57 +0100

    Corrección del print de SolucionAlmacenes

--- src/ejercicio1/SolucionAlmacen.java
+++ src/ejercicio1/SolucionAlmacen.java
@@ -1,5 +1,6 @@
 package ejercicio1;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,9 +45,44 @@ public class SolucionAlmacen {
 
 	@Override
 	public String toString() {
-		return solucion.entrySet().stream().map(p -> "P" + p.getKey().producto() + ": Almacen " + p.getValue())
-				.collect(Collectors.joining("\n",
-						"Reparto de productos y almacen en el que se coloca cada uno de ellos:\n",
-						String.format("\nProductos colocados: %d", numproductos)));
+		String reparto = solucion.entrySet().stream()
+			.sorted(Comparator.comparing(e -> e.getKey().producto()))
+			.map(p -> p.getKey().producto() + ": Almacen " + p.getValue()
+				+ " (" + p.getKey().metroscubicosrequeridos() + "/"
+				+ DatosAlmacenes.getMetrosCubicosAlmacen(p.getValue()) + ")")
+			.collect(Collectors.joining("\n",
+				"Reparto de productos y almacen en el que se coloca cada uno de ellos:\n\n",
+				"\n"));
+
+		// Calcular ocupación por almacén
+		Map<Integer, Integer> ocupacion = solucion.entrySet().stream()
+			.collect(Collectors.groupingBy(
+				e -> e.getValue(),
+				Collectors.summingInt(e -> e.getKey().metroscubicosrequeridos())
+			));
+
+		// Resumen por almacén
+		String resumen = ocupacion.entrySet().stream()
+			.sorted(Map.Entry.comparingByKey())
+			.map(e -> String.format("Almacén %d → %d / %d",
+				e.getKey(),
+				e.getValue(),
+				DatosAlmacenes.getMetrosCubicosAlmacen(e.getKey())))
+			.collect(Collectors.joining("\n"));
+
+		// Totales generales
+		int volumenTotal = ocupacion.values().stream().mapToInt(Integer::intValue).sum();
+		int capacidadTotal = 0;
+		for (int i = 0; i < DatosAlmacenes.getNumAlmacenes(); i++) {
+			capacidadTotal += DatosAlmacenes.getMetrosCubicosAlmacen(i);
+		}
+
+		String totales = String.format(
+			"\n\nProductos colocados: %d\nVolumen ocupado: %d / %d",
+			numproductos, volumenTotal, capacidadTotal);
+
+		return reparto +"\n"+ resumen + totales;
 	}
+
+
 }
