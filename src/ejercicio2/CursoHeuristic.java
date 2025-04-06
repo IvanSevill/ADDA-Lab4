63565ac9b96745cc86b1c0219239ea1fe9a950c0
Ivan Sevillano <ivansevillano2005@gmail.com>
2025-04-06 18:56:59 +0200

    Add comments and reformat heuristic method in CursoHeuristic.java
    - Add comments explaining the heuristic method
    - Reformat method signature for better readability
    - Adjust indentation in the heuristic method
    - Ensure consistent code style
    - Improve code clarity and maintainability

--- src/ejercicio2/CursoHeuristic.java
+++ src/ejercicio2/CursoHeuristic.java
@@ -5,12 +5,16 @@ import java.util.function.Predicate;
 import common.DatosCursos;
 
 public class CursoHeuristic {
-	public static Double heuristic(CursoVertexInterface v1, Predicate<CursoVertexInterface> goal, CursoVertexInterface v2) {
+	// Maxima relevancia que puedo llegar a conseguir en los cursos que me quedan en
+	// caso de que los escoja todos
+	public static Double heuristic(CursoVertexInterface v1, Predicate<CursoVertexInterface> goal,
+			CursoVertexInterface v2) {
 		Double h = 0.;
 		if (v1.indice() < DatosCursos.getNumCursos()) {
 			for (int i = v1.indice(); i < DatosCursos.getNumCursos(); i++) {
-			    h += DatosCursos.getCurso(i).relevancia();
+				h += DatosCursos.getCurso(i).relevancia();
 			}
-		} 
-		return h;	}
+		}
+		return h;
+	}
 }
